3c366a78509f0bfcf83a4544adfd77d6e029e97d
Anthony <anthony.speringcga@mymail.champlain.edu>
2022-07-26 09:33:22 -0400

    Added Project

---
+++ 2D/Assets/Escape.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class Escape : MonoBehaviour
+{
+    public GameObject Text1;
+    public GameObject Text2;
+    public GameObject Game;
+
+    private void Awake()
+    {
+        Text1.SetActive(true);
+        Invoke("text", 2.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    void text()
+    {
+        Text1.SetActive(false);
+        Text2.SetActive(true);
+        Invoke("text2", 7.0f);
+    }
+    void text2()
+    {
+        Text2.SetActive(false);
+        Game.SetActive(true);
+    }
+
+}
---
+++ 2D/Assets/Move.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 10.0f;
+    public float jumpHeight = 0;
+    void Start()
+    {
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        jumpHeight = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpHeight = 25;   
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+    }
+}
+
