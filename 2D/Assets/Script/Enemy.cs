4ded6f5e006f5b4190643d8349da90842f5a9756
Anthony <anthony.speringcga@mymail.champlain.edu>
2022-07-29 11:45:47 -0400

    tfrr

--- 2D/Assets/Script/Enemy.cs
+++ 2D/Assets/Script/Enemy.cs
@@ -5,30 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject target;
-    public float Speed;
-    private float distance;
-    public float Max;
-    public Vector2 height;
-    public Animator anim;
-
-    PatrolingEnemys patrolingEnemys;
-    private void Start()
+    public float speed = 20.0f;
+    public float minDist = 1f;
+    public Transform target;
+    void Start()
     {
-        patrolingEnemys = GetComponent<PatrolingEnemys>();
-        target = GameObject.FindGameObjectWithTag("Player");
-    }
-    
-    private void Update()
-    {
-        bool moving = true;
-        distance = Vector2.Distance(target.transform.position, transform.position);
-        if (distance <= Max)
+        if (target == null)
         {
-            transform.position = Vector2.MoveTowards(transform.position, height, Speed * Time.deltaTime);
-            //wall.gameObject.SetActive(false);
-            patrolingEnemys.enabled = false;
+            if (GameObject.FindWithTag("Player") != null)
+            {
+                target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            }
         }
-        anim.SetBool("Walking", moving);
+    }
+
+    void Update()
+    {
+        if (target == null)
+            return;
+        transform.LookAt(target);
+        float distance = Vector2.Distance(transform.position, target.position);
+        if (distance > minDist)
+            transform.position += transform.forward * speed * Time.deltaTime;
+    }
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
     }
 }
